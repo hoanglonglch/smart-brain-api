0255367aca3da5af99851a49ce0e30fc608a137c
hoanglonglch <hoanglonglch@gmail.com>
2019-08-11 22:28:08 +0700

    Init docker project

---
+++ postgres/deploy_schemas.sql
@@ -0,0 +1,4 @@
+-- Deploy fresh database table
+
+\i '/docker-entrypont-initdb.d/tables/users.sql'
+\i '/docker-entrypont-initdb.d/tables/login.sql'
---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+  client: 'pg',
+  connection: process.env.POSTGRES_URI
+});
+
+const app = express();
+console.log('hehe sdasdss');
+
+app.use(cors())
+app.use(bodyParser.json());
+
+app.get('/', (req, res)=> { res.send('hello 123') })
+app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
+app.put('/image', (req, res) => { image.handleImage(req, res, db)})
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+
+app.listen(3000, ()=> {
+  console.log('app is running on port 3000');
+})
